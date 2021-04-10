144e5caa847254974a0a689408f049d4dc1d3584
hoss <agcacho22@gmail.com>
2021-04-10 02:15:26 -0700

    added example select card state

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -11,10 +11,25 @@ export default {
 } as Meta;
 
 export const Cards: Story = () => (
-  <div style={{ display: "flex" }}>
-    <Card cardId="002pikapuffUC.png" />
+  <div style={{ display: "flex", padding: 0 }}>
+    <Card
+      cardId="002pikapuffUC.png"
+      onClick={() => {
+        console.log("clicked");
+      }}
+    />
     <Card cardId="015rashscratchumL.png" />
     <Card cardId="016babymeownautML.png" />
     <Card />
+    <div
+      style={{
+        backgroundColor: "black",
+        flex: 1,
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        zIndex: -1,
+      }}
+    ></div>
   </div>
 );
--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,20 +1,70 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useAppSelector, useAppDispatch } from "../../providers/state/hooks";
 import { asyncFetchIds } from "../../providers/state/CarouselState";
 
 import Card from "../Card";
+import zIndex from "@material-ui/core/styles/zIndex";
 
 const Carousel: React.FC = () => {
   const carousel = useAppSelector((state) => state.carousel);
+  const [zState, setZState] = useState(carousel.nfts?.map((nft) => 0));
+  const [opacity, setOpacity] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(-1);
+
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(asyncFetchIds(1));
-  }, []);
+  }, [dispatch]);
+
+  const selectCard = (index: number) => {
+    const updatedState = zState;
+    let foundCard = false;
+
+    if (activeIndex === index) {
+      //unfocus
+      setOpacity(0);
+      updatedState[activeIndex] = 0;
+      setZState(updatedState);
+      setActiveIndex(-1);
+      return;
+    }
+
+    if (updatedState && updatedState.length >= index) {
+      updatedState[index] = 2;
+      foundCard = true;
+    }
+
+    setZState(updatedState);
+    if (foundCard) {
+      setActiveIndex(index);
+      setOpacity(1);
+    }
+  };
+
   return (
     <div style={{ display: "flex" }}>
-      {carousel?.nfts?.map((nft) => (
-        <Card cardId={nft.imageUrl} />
+      <div
+        style={{
+          backgroundColor: "black",
+          flex: 1,
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          zIndex: 1,
+          opacity: opacity,
+          pointerEvents: opacity === 0 ? "none" : "visible",
+          backgroundAttachment: "fixed",
+        }}
+      />
+      {carousel?.nfts?.map((nft, index) => (
+        <Card
+          cardId={nft.imageUrl}
+          key={index.toString()}
+          style={{ zIndex: zState[index] }}
+          onClick={() => selectCard(index)}
+        />
       ))}
     </div>
   );
